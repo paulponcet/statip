9eb0e2a1b9efa0b0d168f948204251d0fca642fc
paulponcet <paulponcet@yahoo.fr>
2019-11-17 21:04:04 +0000

    Update 'mfv()', now powered by 'tableNA()'

--- R/mfv.R
+++ R/mfv.R
@@ -2,7 +2,7 @@
 #' Most frequent value(s)
 #'
 #' @description
-#' The function \code{mfv} returns the most frequent value(s) (or mode(s))
+#' The function \code{mfv()} returns the most frequent value(s) (or mode(s))
 #' found in a vector.
 #' The function \code{mfv1} returns the first of these values, so that 
 #' \code{mfv1(x)} is identical to \code{mfv(x)[[1L]]}. 
@@ -14,7 +14,7 @@
 #' values treatment. 
 #' 
 #' @note 
-#' \code{mfv} calls the function \code{\link{tabulate}}. 
+#' \code{mfv()} calls the function \code{\link[base]{tabulate}}. 
 #' 
 #' @references 
 #' \itemize{ 
@@ -28,12 +28,12 @@
 #' logical).
 #' \code{x} is to come from a discrete distribution. 
 #' 
-#' @param na.rm 
+#' @param na_rm 
 #' logical. If \code{TRUE}, missing values do not interfer 
 #' with the result, see 'Details'. 
 #' 
 #' @param ...
-#' Additional arguments (not used). 
+#' Additional arguments (currently not used). 
 #'
 #' @return
 #' The function \code{mfv} returns a vector of the same type as \code{x}.
@@ -46,47 +46,118 @@
 #'
 #' @examples
 #' # Basic examples:
-#' mfv(c(3, 3, 3, 2, 4))            # 3
-#' mfv(c(TRUE, FALSE, TRUE))        # TRUE
-#' mfv(c("a", "a", "b", "a", "d"))  # "a"
+#' mfv(integer(0))                      # NaN
+#' mfv(c(3, 3, 3, 2, 4))                # 3
+#' mfv(c(TRUE, FALSE, TRUE))            # TRUE
+#' mfv(c("a", "a", "b", "a", "d"))      # "a"
 #'
-#' mfv(c("a", "a", "b", "b", "d"))  # c("a", "b")
-#' mfv1(c("a", "a", "b", "b", "d")) # "a"
+#' mfv(c("a", "a", "b", "b", "d"))      # c("a", "b")
+#' mfv1(c("a", "a", "b", "b", "d"))     # "a"
 #'
 #' # With missing values: 
-#' mfv(c(3, 3, 3, 2, NA))           # 3
-#' mfv(c(3, 3, 2, NA))              # NA
-#' mfv(c(3, 3, 2, NA), na.rm = TRUE)# 3
+#' mfv(c(3, 3, 3, 2, NA))               # 3
+#' mfv(c(3, 3, 2, NA))                  # NA
+#' mfv(c(3, 3, 2, NA), na_rm = TRUE)    # 3
+#' mfv(c(3, 3, 2, 2, NA))               # NA
+#' mfv(c(3, 3, 2, 2, NA), na_rm = TRUE) # c(2, 3)
+#' mfv1(c(3, 3, 2, 2, NA), na_rm = TRUE)# 2
 #' 
 #' # With only missing values: 
 #' mfv(c(NA, NA))                   # NA
-#' mfv(c(NA, NA), na.rm = TRUE)     # NaN
+#' mfv(c(NA, NA), na_rm = TRUE)     # NaN
+#' 
+#' # With factors
+#' mfv(factor(c("a", "b", "a")))
+#' mfv(factor(c("a", "b", "a", NA)))
+#' mfv(factor(c("a", "b", "a", NA)), na_rm = TRUE)
 #' 
 mfv <-
 function(x,
-         na.rm = FALSE, 
          ...)
 {
-  cl <- typeof(x) #class(x)[[1L]]
-  n <- length(x)
-  a <- sum(is.na(x))
-  if (na.rm) n <- n - a
-  if (n == 0L) return(NaN)
+  UseMethod("mfv")
+}
+
+
+# TODO: ne marche pas avec , devrait renvoyer NA
+
+#' @export
+#' @rdname mfv
+#' 
+mfv.default <- 
+function(x,
+         na_rm = FALSE, 
+         ...)
+{
+  mfv(tableNA(x), na_rm = na_rm, ...)
   
-  f  <- factor(x)
-  tf <- tabulate(f)
-  n1 <- max(tf)
-  
-  if (!na.rm && a > 0L) {
-    n2 <- ifelse(length(tf) > 1L, max(tf[tf!=n1]), 0L)
-    if (n2 + a >= n1) return(as.vector(NA, mode = cl))
+  # cl <- typeof(x) #class(x)[[1L]]
+  # n <- length(x)
+  # a <- sum(is.na(x))
+  # if (na_rm) n <- n - a
+  # if (n == 0L) return(NaN)
+  # 
+  # f  <- factor(x)
+  # lf <- levels(f)
+  # tf <- tabulate(f)
+  # wf <- which.max(tf)
+  # n1 <- tf[wf] #max(tf)
+  # v  <- lf[tf == n1]
+  # 
+  # if (!na_rm && a > 0L) {
+  #   n2 <- ifelse(length(tf) > 1L, max(tf[-wf]), 0L)
+  #   if (n2 + a >= n1) v <- NA
+  # }
+  # 
+  # if (is.factor(x)) {
+  #   return(factor(v, levels = lf))
+  # } else {
+  #   return(as.vector(v, mode = cl))
+  # }
+}
+
+
+#' @export
+#' @rdname mfv
+#' 
+mfv.tableNA <- 
+function(x, 
+         na_rm = FALSE, 
+         ...)
+{
+  l <- list(...)
+  if (!is.null(l$na.rm)) {
+    message("argument 'na.rm' is soft-deprecated, please start using 'na_rm' instead")
+    na_rm <- l$na.rm
   }
-  lf <- levels(f)[tf == n1]
   
-  if (is.factor(x)) {
-    return(as.factor(lf))
+  tf <- x
+  
+  a <- tf["<NA>"]
+  if (na_rm || is.na(a)) {
+    a <- 0L
+  }
+  tf["<NA>"] <- 0L
+  
+  if (length(tf) == 1L && a == 0L) return(NaN)
+
+  wf <- which.max(tf)
+  n1 <- tf[wf]
+  v  <- names(tf)[tf == n1]
+  
+  if (length(tf) == 1L && !na_rm) {
+    v <- NA
+  }
+    
+  if (!na_rm && a > 0L) {
+    n2 <- ifelse(length(tf) > 1L, max(tf[-wf]), 0L)
+    if (n2 + a >= n1) v <- NA
+  }
+  
+  if (attr(x, "is_factor_x")) {
+    return(factor(v, levels = levels(x)))
   } else {
-    return(as.vector(lf, mode = cl))
+    return(as.vector(v, mode = attr(x, "type_of_x")))
   }
 }
 
@@ -96,8 +167,8 @@ function(x,
 #'
 mfv1 <-
 function(x, 
-         na.rm = FALSE, 
+         na_rm = FALSE, 
          ...)
 {
-  mfv(x, na.rm = na.rm, ...)[[1L]]
+  mfv(x, na_rm = na_rm, ...)[[1L]]
 }
