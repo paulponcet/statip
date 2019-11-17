373f67e1a7a6e7bf0b1cf7d43577f4d566a422b6
paulponcet <paulponcet@yahoo.fr>
2019-11-17 20:56:04 +0000

    Add unit tests

--- tests/testthat/test-mfv.R
+++ tests/testthat/test-mfv.R
@@ -1,6 +1,7 @@
 context("Test 'mfv()' function")
 
 test_that("'mfv()' returns the most frequent value(s)", {
+  expect_identical(mfv(integer(0)), NaN)
   expect_identical(mfv(c(3, 3, 3, 2, 4)), 3)
   expect_identical(mfv(c(TRUE, FALSE, TRUE)), TRUE)
   expect_identical(mfv(c("a", "a", "b", "a", "d")), "a")
@@ -11,7 +12,19 @@ test_that("'mfv()' returns the most frequent value(s)", {
 test_that("'mfv()' works with missing values", {
   expect_identical(mfv(c(3, 3, 3, 2, NA)), 3)
   expect_identical(mfv(c(3, 3, 2, NA)), NA_real_)
-  expect_identical(mfv(c(3, 3, 2, NA), na.rm = TRUE), 3)
-  expect_identical(mfv(c("a", "a", "b", "b", "d")), c("a", "b"))
-  expect_identical(mfv(c(NA, NA), na.rm = TRUE), NaN)
+  expect_identical(mfv(c(3, 3, 2, NA), na_rm = TRUE), 3)
+  expect_identical(mfv(c(3, 3, 2, 2, NA)), NA_real_)
+  expect_identical(mfv(c(3, 3, 2, 2, NA), na_rm = TRUE), c(2, 3))
+  
+  expect_identical(mfv(c(NA, NA)), NA)
+  expect_identical(mfv(c(NA, NA), na_rm = TRUE), NaN)
+})
+
+test_that("'mfv()' works with factors", {
+  expect_identical(mfv(factor(c("a", "b", "a"))), 
+                   factor("a", levels = c("a", "b")))
+  expect_identical(mfv(factor(c("a", "b", "a", NA))), 
+                   factor(NA, levels = c("a", "b")))
+  expect_identical(mfv(factor(c("a", "b", "a", NA)), na_rm = TRUE), 
+                   factor("a", levels = c("a", "b")))
 })
